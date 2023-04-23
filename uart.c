49d0b091279bd7b1f3a5ab174b7cb22749f9f883
Khalid Bin Walid <kbwalid26@gmail.com>
2023-04-23 11:19:56 +0900

    use setbaud.d for baud calculation

--- uart.c
+++ uart.c
@@ -1,21 +1,25 @@
 #include <avr/io.h>
 
-static inline void uart_init(uint32_t baud) {
-    const uint32_t ubrr = (F_CPU / 4 / baud - 1) / 2;
-    UBRR0H = (uint8_t)(ubrr >> 8);
-    UBRR0L = (uint8_t)(ubrr >> 0);
+static void uart_init() {
+#define BAUD 115200
+#define BAUD_TOL 3
+#include <util/setbaud.h>
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+#if USE_2X
     UCSR0A = (1 << U2X0);
+#endif
     UCSR0B = (1 << RXEN0) | (1 << TXEN0);
     UCSR0C = (3 << UCSZ00);
 }
 
-static inline void uart_tx(uint8_t data) {
+static void uart_tx(uint8_t data) {
     while (!(UCSR0A & (1 << UDRE0)))
         ;
     UDR0 = data;
 }
 
-static inline uint8_t uart_rx() {
+static uint8_t uart_rx() {
     while (!(UCSR0A & (1 << RXC0)))
         ;
     return UDR0;
@@ -25,7 +29,7 @@ int main() {
     // configure LED pin as output
     DDRB |= 0x20;
 
-    uart_init(115200);
+    uart_init();
     while (1) {
         uart_tx(uart_rx());
         // toggle LED
