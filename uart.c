1aec588149affc3bf352131bdc08505024f30ac3
Khalid Bin Walid <kbwalid26@gmail.com>
2023-04-29 12:53:06 +0900

    add setup for printf, always sync uart, process some special char

--- uart.c
+++ uart.c
@@ -1,6 +1,17 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <stdio.h>
+
+static void uart_tx(uint8_t data);
+static int uart_putchar(char c, FILE *stream) {
+    (void)stream;
+    uart_tx(c);
+    return 0;
+}
+
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL,
+        _FDEV_SETUP_WRITE);
 
 void uart_init() {
 #define BAUD 115200
@@ -16,23 +27,38 @@ void uart_init() {
 }
 
 static void uart_tx(uint8_t data) {
-#ifdef UART_SYNC_TX
     while (!(UCSR0A & (1 << UDRE0)))
         ;
-#endif
     UDR0 = data;
 }
 
 static uint8_t uart_rx() {
-#ifdef UART_SYNC_RX
     while (!(UCSR0A & (1 << RXC0)))
         ;
-#endif
     return UDR0;
 }
 
+static void uart_puts(const char *str) {
+    for (const char *c = str; *c; c++)
+        uart_tx(*c);
+}
+
+#define CTRL(c) (c & 0x1f)
+
 ISR(USART_RX_vect) {
-    uart_tx(uart_rx());
+    char c = uart_rx();
+    // printf("%d\r\n", c);
+    switch (c) {
+    case CTRL('a'):
+        uart_puts("\033[G");
+        break;
+    case CTRL('l'):
+        uart_puts("\033[H\033[J");
+        break;
+    default:
+        uart_tx(c);
+        break;
+    }
     // toggle LED
     PINB |= 0x20;
 }
@@ -41,6 +67,7 @@ int main() {
     // configure LED pin as output
     DDRB |= 0x20;
 
+    stdout = &mystdout;
     uart_init();
     sei();
     while (1) {
